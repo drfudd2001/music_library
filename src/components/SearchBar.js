5cdd336e43e9ed9fa8b2a9fdc3a06a7bd2414fa2
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-05-15 20:52:25 -0500

    Completed additional work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 function SearchBar(props){
-    // We can comment out our searchTerm state variable as we are not using it!
     // let [searchTerm, setSearchTerm] = useState('')
 
     return (
