ce5e0d367bef4d4e821be2fa6409284e0595b623
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-05-16 18:01:59 -0500

    Further updates

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,45 +1,37 @@
-import { useState, useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import {useParams, useHistory} from 'react-router-dom';
 
-function AlbumView() {
-    const navigate = useNavigate()
+const AlbumView = (props) => {
     const { id } = useParams()
+    const history = useHistory()
     const [ albumData, setAlbumData ] = useState([])
-
+    
     useEffect(() => {
-        const API_URL = `http://localhost:4000/song/${id}`
         const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
             const response = await fetch(API_URL)
             const resData = await response.json()
             setAlbumData(resData.results)
         }
         fetchData()
-    },[id])
-
-    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
-
-    const renderSongs = justSongs.map((song, i) => {
-        return (
-            <div key={i}>
-                <p>{song.trackName}</p>
-            </div>
-        )
-    })
+    }, [id])
 
     const navButtons = () => {
-        return(
+        return (
             <div>
-                <button onClick={() => navigate(-1)}>Back</button>
-                |
-                <button onClick={() => navigate('/')}>Home</button>
+                <button onClick={() => {history.push('/')}}>Home</button> | <button onClick={() => history.goBack()}>Back</button>
             </div>
         )
     }
 
+    const allAlbums =   albumData.filter(entity => entity.kind === 'song')
+                        .map((album, i) => { return (<div key={i}>{album.trackName}</div>)})
+
     return (
         <div>
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <p>loading...</p>}
             {navButtons()}
-            {renderSongs}
+            {allAlbums}
         </div>
     )
 }
